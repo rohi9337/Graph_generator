c998f0d3b8711baa3e2f51d9a556c3257b025794
Abel Leul <abelleul2015@gmail.com>
2024-05-26 00:48:53 +0300

    modified the Django views so that the graph is not saved to the database but is generated and displayed to the user as an image

--- graph/urls.py
+++ graph/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name = 'graph'
 urlpatterns = [
     path('', views.graph_form, name='graph_form'),
-    path('view/<int:pk>/', views.graph_view, name='graph_view'),
-    path('save/<int:pk>/', views.save_graph, name='save_graph'),
+    path('save/', views.save_graph, name='save_graph'),
 ]
--- graph/views.py
+++ graph/views.py
@@ -2,10 +2,9 @@ import io
 import base64
 import matplotlib.pyplot as plt
 import numpy as np
-from django.conf import settings
-from django.shortcuts import render, redirect
-from .models import GraphData
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import GraphData
 
 
 def graph_form(request):
@@ -19,85 +18,76 @@ def graph_form(request):
         graph_type = request.POST['graph_type']
         grid = 'grid' in request.POST  # Check if the 'grid' checkbox is checked
 
-        graph_data = GraphData(
-            title=title,
-            x_label=x_label,
-            y_label=y_label,
-            x_values=x_values,
-            y_values=y_values,
-            color_preference=color_preference,
-            graph_type=graph_type,
-            grid=grid
-        )
-        graph_data.save()
+        x_values = np.array([float(x) for x in x_values.split(',')])
+        y_values = np.array([float(y) for y in y_values.split(',')])
 
-        return redirect('graph:graph_view', pk=graph_data.pk)
+        font1 = {'family': 'serif', 'color': 'blue', 'size': 20}
+        font2 = {'family': 'serif', 'color': 'darkred', 'size': 15}
+
+        # Create the chart
+        fig, ax = plt.subplots(figsize=(8, 6))
+        if graph_type == 'line':
+            ax.plot(x_values, y_values, color=color_preference)
+        elif graph_type == 'scatter':
+            ax.scatter(x_values, y_values, color=color_preference)
+        elif graph_type == 'bar':
+            ax.bar(x_values, y_values, color=color_preference)
+        elif graph_type == 'histogram':
+            ax.hist(x_values, bins=int(y_values[0]), color=color_preference)
+        ax.set_xlabel(x_label, fontdict=font2)
+        ax.set_ylabel(y_label, fontdict=font2)
+        ax.set_title(title, fontdict=font1)
+        if grid:
+            ax.grid(True)
+
+        # Save the chart to a buffer
+        buffer = io.BytesIO()
+        fig.savefig(buffer, format='png')
+        buffer.seek(0)
+        image_data = buffer.getvalue()
+        buffer.close()
+
+        # Encode the image to base64 for display in the template
+        graph = base64.b64encode(image_data).decode('utf-8')
+
+        context = {'graph': graph, 'title': title,
+                   'x_label': x_label, 'y_label': y_label}
+        return render(request, 'graph_view.html', context)
 
     return render(request, 'graph_form.html')
 
 
-def graph_view(request, pk):
-    graph_data = GraphData.objects.get(pk=pk)
+def save_graph(request):
+    print(request.POST)
+    title = request.POST['title']
+    x_label = request.POST['x_label']
+    y_label = request.POST['y_label']
+    x_values = request.POST['x_values']
+    y_values = request.POST['y_values']
+    color_preference = request.POST['color_preference']
+    graph_type = request.POST['graph_type']
+    grid = 'grid' in request.POST
 
-    x_values = np.array([float(x) for x in graph_data.x_values.split(',')])
-    y_values = np.array([float(y) for y in graph_data.y_values.split(',')])
+    x_values = np.array([float(x) for x in x_values.split(',')])
+    y_values = np.array([float(y) for y in y_values.split(',')])
 
-    font1 = {'family': 'serif', 'color': 'blue', 'size': 20}
-    font2 = {'family': 'serif', 'color': 'darkred', 'size': 15}
-
-    # Create the chart
     fig, ax = plt.subplots(figsize=(8, 6))
-    if graph_data.graph_type == 'line':
-        ax.plot(x_values, y_values, color=graph_data.color_preference)
-    elif graph_data.graph_type == 'scatter':
-        ax.scatter(x_values, y_values, color=graph_data.color_preference)
-    elif graph_data.graph_type == 'bar':
-        ax.bar(x_values, y_values, color=graph_data.color_preference)
-    elif graph_data.graph_type == 'histogram':
-        ax.hist(x_values, bins=int(y_values[0]),
-                color=graph_data.color_preference)
-    ax.set_xlabel(graph_data.x_label, fontdict=font2)
-    ax.set_ylabel(graph_data.y_label, fontdict=font2)
-    ax.set_title(graph_data.title, fontdict=font1)
-    if graph_data.grid:
-        ax.grid(True)
-
-    # Save the chart to a buffer
-    buffer = io.BytesIO()
-    fig.savefig(buffer, format='png')
-    buffer.seek(0)
-    image_data = buffer.getvalue()
-    buffer.close()
-
-    # Encode the image to base64 for display in the template
-    graph = base64.b64encode(image_data).decode('utf-8')
-
-    context = {'graph': graph, 'graph_data': graph_data}
-    return render(request, 'graph_view.html', context)
-
-
-def save_graph(request, pk):
-    graph_data = GraphData.objects.get(pk=pk)
-
-    # Create the chart
-    fig, ax = plt.subplots(figsize=(8, 6))
-    if graph_data.graph_type == 'line':
-        ax.plot(np.array([float(x) for x in graph_data.x_values.split(',')]),
-                np.array([float(y) for y in graph_data.y_values.split(',')]),
-                color=graph_data.color_preference)
-    elif graph_data.graph_type == 'scatter':
-        ax.scatter(np.array([float(x) for x in graph_data.x_values.split(',')]),
-                   np.array([float(y)
-                            for y in graph_data.y_values.split(',')]),
-                   color=graph_data.color_preference)
-    ax.set_xlabel(graph_data.x_label)
-    ax.set_ylabel(graph_data.y_label)
-    ax.set_title(graph_data.title)
-    if graph_data.grid:
+    if graph_type == 'line':
+        ax.plot(x_values, y_values, color=color_preference)
+    elif graph_type == 'scatter':
+        ax.scatter(x_values, y_values, color=color_preference)
+    elif graph_type == 'bar':
+        ax.bar(x_values, y_values, color=color_preference)
+    elif graph_type == 'histogram':
+        ax.hist(x_values, bins=int(y_values[0]), color=color_preference)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+    if grid:
         ax.grid(True)
 
     # Save the chart to a file
-    file_name = f'graph_{graph_data.pk}.png'
+    file_name = 'generated_graph.png'
     fig.savefig(file_name, format='png')
 
     # Serve the file as a response
